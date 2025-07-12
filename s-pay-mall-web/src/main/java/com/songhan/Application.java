70713220652a6db77e40ca14a620d7234628d2aa
nininini820 <2403182464@qq.com>
2025-07-12 19:20:09 -0400

    wechat portal

--- s-pay-mall-web/src/main/java/com/songhan/Application.java
+++ s-pay-mall-web/src/main/java/com/songhan/Application.java
@@ -1,4 +1,16 @@
 package com.songhan;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Configurable
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
 }
